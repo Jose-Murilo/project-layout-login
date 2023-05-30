244628ab8a1913e4e2c98a7d0d1af3eb48e898e2
Jose-Murilo <murilojucas360@hotmail.com>
2023-05-30 14:14:12 -0300

    added if is Authenticated in page ChangePassword

--- src/app/changepassword/page.tsx
+++ src/app/changepassword/page.tsx
@@ -17,6 +17,8 @@ type AlterPasswordProps = {
 }
 
 export default function ChangePassword() {
+  const isAuthenticated =
+    localStorage.getItem('isAuthenticated') === 'Authenticated'
   const {
     register,
     handleSubmit,
@@ -56,112 +58,116 @@ export default function ChangePassword() {
     }
   }
 
-  return (
-    <div className="md:grid min-h-screen grid-cols-2">
-      <div className="">
-        <form
-          onSubmit={handleSubmit(alterPassword)}
-          className="flex leading-relaxed bg-changePassword justify-center items-center min-h-screen"
-        >
-          <div className="flex flex-col gap-5">
-            <section>
-              <h1 className="text-white text-[32px] ml-2 font-bold">
-                Alterar senha
-              </h1>
-            </section>
+  if (!isAuthenticated) {
+    return (
+      <div className="md:grid min-h-screen grid-cols-2">
+        <div className="">
+          <form
+            onSubmit={handleSubmit(alterPassword)}
+            className="flex leading-relaxed bg-changePassword justify-center items-center min-h-screen"
+          >
+            <div className="flex flex-col gap-5">
+              <section>
+                <h1 className="text-white text-[32px] ml-2 font-bold">
+                  Alterar senha
+                </h1>
+              </section>
 
-            <section>
-              <div className="relative">
-                <IconPadlock
-                  width={23}
-                  height={23}
-                  className="absolute top-[11px] left-2"
-                />
-                <input
-                  {...register('newPassword', { required: true })}
-                  type={isVisible ? 'text' : 'password'}
-                  placeholder="Senha"
-                  className="bg-white-transparent text-sm px-8 placeholder:text-sm placeholder:text-black p-3 leading-relaxed w-[300px] sm:w-[350px] rounded-full"
-                />
-                {errors.newPassword && (
-                  <span className="absolute text-[11px] bottom-[-18px] left-2 text-red-600">
-                    Este campo é obrigatorio.
-                  </span>
-                )}
-
-                {isVisible ? (
-                  <IconEye
-                    width={22}
-                    height={22}
-                    onClick={() => setIsVisible((prev) => !prev)}
-                    className="absolute top-[13px] right-5 cursor-pointer"
+              <section>
+                <div className="relative">
+                  <IconPadlock
+                    width={23}
+                    height={23}
+                    className="absolute top-[11px] left-2"
                   />
-                ) : (
-                  <IconEyeInvisible
-                    width={22}
-                    height={22}
-                    onClick={() => setIsVisible((prev) => !prev)}
-                    className="absolute top-[13px] right-5 cursor-pointer"
+                  <input
+                    {...register('newPassword', { required: true })}
+                    type={isVisible ? 'text' : 'password'}
+                    placeholder="Senha"
+                    className="bg-white-transparent text-sm px-8 placeholder:text-sm placeholder:text-black p-3 leading-relaxed w-[300px] sm:w-[350px] rounded-full"
                   />
-                )}
-              </div>
-            </section>
+                  {errors.newPassword && (
+                    <span className="absolute text-[11px] bottom-[-18px] left-2 text-red-600">
+                      Este campo é obrigatorio.
+                    </span>
+                  )}
 
-            <section>
-              <div className="relative">
-                <IconPadlock
-                  width={23}
-                  height={23}
-                  className="absolute top-[11px] left-2"
-                />
-                <input
-                  {...register('confirmNewPassword', { required: true })}
-                  type={isVisible ? 'text' : 'password'}
-                  placeholder="Confirme a nova senha"
-                  className="bg-white-transparent focus:outline-black text-sm px-8 placeholder:text-sm placeholder:text-black p-3 leading-relaxed w-[300px] sm:w-[350px] rounded-full"
-                />
-                {errors.confirmNewPassword && (
-                  <span className="absolute text-[11px] bottom-[-18px] left-2 text-red-600">
-                    Este campo é obrigatorio.
-                  </span>
-                )}
+                  {isVisible ? (
+                    <IconEye
+                      width={22}
+                      height={22}
+                      onClick={() => setIsVisible((prev) => !prev)}
+                      className="absolute top-[13px] right-5 cursor-pointer"
+                    />
+                  ) : (
+                    <IconEyeInvisible
+                      width={22}
+                      height={22}
+                      onClick={() => setIsVisible((prev) => !prev)}
+                      className="absolute top-[13px] right-5 cursor-pointer"
+                    />
+                  )}
+                </div>
+              </section>
 
-                {isVisible ? (
-                  <IconEye
-                    width={22}
-                    height={22}
-                    onClick={() => setIsVisible((prev) => !prev)}
-                    className="absolute top-[13px] right-5 cursor-pointer"
+              <section>
+                <div className="relative">
+                  <IconPadlock
+                    width={23}
+                    height={23}
+                    className="absolute top-[11px] left-2"
                   />
-                ) : (
-                  <IconEyeInvisible
-                    width={22}
-                    height={22}
-                    onClick={() => setIsVisible((prev) => !prev)}
-                    className="absolute top-[13px] right-5 cursor-pointer"
+                  <input
+                    {...register('confirmNewPassword', { required: true })}
+                    type={isVisible ? 'text' : 'password'}
+                    placeholder="Confirme a nova senha"
+                    className="bg-white-transparent focus:outline-black text-sm px-8 placeholder:text-sm placeholder:text-black p-3 leading-relaxed w-[300px] sm:w-[350px] rounded-full"
                   />
-                )}
-              </div>
-            </section>
+                  {errors.confirmNewPassword && (
+                    <span className="absolute text-[11px] bottom-[-18px] left-2 text-red-600">
+                      Este campo é obrigatorio.
+                    </span>
+                  )}
 
-            <button className="bg-title transition-all hover:bg-[#4085f0] text-white  p-2 mt-2 w-[300px] sm:w-[350px] rounded-full">
-              Alterar
-            </button>
+                  {isVisible ? (
+                    <IconEye
+                      width={22}
+                      height={22}
+                      onClick={() => setIsVisible((prev) => !prev)}
+                      className="absolute top-[13px] right-5 cursor-pointer"
+                    />
+                  ) : (
+                    <IconEyeInvisible
+                      width={22}
+                      height={22}
+                      onClick={() => setIsVisible((prev) => !prev)}
+                      className="absolute top-[13px] right-5 cursor-pointer"
+                    />
+                  )}
+                </div>
+              </section>
 
-            <Link
-              className="text-[12px] text-center text-white"
-              href={'/signin'}
-            >
-              Volte para a tela de login
-            </Link>
-          </div>
-        </form>
-        <AuthorCredits colorText="white" />
+              <button className="bg-title transition-all hover:bg-[#4085f0] text-white  p-2 mt-2 w-[300px] sm:w-[350px] rounded-full">
+                Alterar
+              </button>
+
+              <Link
+                className="text-[12px] text-center text-white"
+                href={'/signin'}
+              >
+                Volte para a tela de login
+              </Link>
+            </div>
+          </form>
+          <AuthorCredits colorText="white" />
+        </div>
+
+        <div className="hidden md:flex items-center bg-blue-transparent justify-center">
+          <Image src={imgChangePassword} alt="" />
+        </div>
       </div>
-
-      <div className="hidden md:flex items-center bg-blue-transparent justify-center">
-        <Image src={imgChangePassword} alt="" />
-      </div>
-    </div>
-  )
+    )
+  } else {
+    window.location.href = '/'
+  }
 }
